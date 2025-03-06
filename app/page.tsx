cfd397e2211026537f9f129ae820229a56836e1d
ElFuchito <85550012+ElFuchito@users.noreply.github.com>
2025-03-06 01:16:17 +0100

    fix: bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Autoplay, EffectCreative, Keyboard, Pagination } from 'swiper/modules';
